54514c5ad669d1059248ce8e313348e2288246a5
Maximilian Kossek <mkossek@asu.edu>
2019-10-02 09:48:06 -0700

    Initial commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,76 @@
+/**
+ * Name:		Maximilian Kossek
+ * ASUID:		1211229276
+ * Assignment:	2
+ * <p>
+ * Defines the AddingMachine class and method for computing 
+ * arithmetic operations: add, subtract.
+ * </p>
+ */
+
+package cse360assign2;
+
+/**
+ * AddingMachine is a class that keeps track of a total and
+ * manipulates the total by <code>add</code> and 
+ * <code>subtract</code> methods.
+ * @author      Maximilian Kossek
+ * @version     1.0
+ * @since       1.0
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor method to initialize the total to 0.
+	 * @since 1.0
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total of an object of the AddingMachine class.
+	 * @return <code>int total</code>.
+	 * @since 1.0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Addition method.
+	 * @param value, the integer to add
+	 * @since 1.0
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtraction method.
+	 * @param value, the integer to subtract
+	 * @since 1.0
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns the toString of an object of the AddingMachine class.
+	 * @return <code>String</code>.
+	 * @since 1.0
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clear method.
+	 * @since 1.0
+	 */
+	public void clear() {
+	
+	}
+}
